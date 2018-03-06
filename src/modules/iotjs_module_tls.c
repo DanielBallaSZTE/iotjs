ca121935eb8b3e5ba63256df71bf6db4e399a1dd
Daniel Balla <dballa@inf.u-szeged.hu>
2018-03-06 16:04:57 +0100

    other

--- src/modules/iotjs_module_tls.c
+++ src/modules/iotjs_module_tls.c
@@ -14,14 +14,14 @@
  */
 
 #include "iotjs_module_tls.h"
+#include "iotjs_module_buffer.h"
 
 #include "stdarg.h"
 
 
 IOTJS_DEFINE_NATIVE_HANDLE_INFO_THIS_MODULE(tls);
 
-static void iotjs_tls_destroy(iotjs_tls_t* tls_data) {
-  mbedtls_net_free(&tls_data->server_fd);
+static void iotjs_tls_destroy(iotjs_tls_t *tls_data) {
   mbedtls_x509_crt_free(&tls_data->cacert);
   mbedtls_ssl_free(&tls_data->ssl);
   mbedtls_ssl_config_free(&tls_data->conf);
@@ -31,13 +31,12 @@ static void iotjs_tls_destroy(iotjs_tls_t* tls_data) {
   IOTJS_RELEASE(tls_data);
 }
 
-static iotjs_tls_t* tls_create(const jerry_value_t jobject) {
-  iotjs_tls_t* tls_data = IOTJS_ALLOC(iotjs_tls_t);
+static iotjs_tls_t *iotjs_tls_create(const jerry_value_t jobject) {
+  iotjs_tls_t *tls_data = IOTJS_ALLOC(iotjs_tls_t);
 
   tls_data->jobject = jobject;
   jerry_set_object_native_pointer(jobject, tls_data, &this_module_native_info);
 
-  mbedtls_net_init(&tls_data->server_fd);
   mbedtls_ssl_init(&tls_data->ssl);
   mbedtls_ssl_config_init(&tls_data->conf);
   mbedtls_x509_crt_init(&tls_data->cacert);
@@ -47,117 +46,527 @@ static iotjs_tls_t* tls_create(const jerry_value_t jobject) {
   return tls_data;
 }
 
+/* Return the number of pending bytes in read and write buffers */
+size_t iotjs_bio_ctrl_pending(BIO *bio) {
+  if (bio == NULL) {
+    return 0;
+  }
 
-JS_FUNCTION(Write) {
-  DJS_CHECK_ARGS(1, string);
-  JS_DECLARE_THIS_PTR(tls, tls_data);
-  iotjs_string_t str = JS_GET_ARG(0, string);
-  int ret = 0;
-  while ((ret = mbedtls_ssl_write(&tls_data->ssl,
-                                  (unsigned char*)(iotjs_string_data(&str)),
-                                  iotjs_string_size(&str))) <= 0) {
-    if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-      iotjs_string_destroy(&str);
-      return JS_CREATE_ERROR(COMMON, "write error");
+  if (bio->type == BIO_MEMORY) {
+    return (size_t)bio->mem_len;
+  }
+
+  /* type BIO_BIO then check paired buffer */
+  if (bio->type == BIO_BIO && bio->pair != NULL) {
+    BIO *pair = bio->pair;
+
+    if (pair->write_idx > 0 && pair->write_idx <= pair->read_idx) {
+      /* in wrap around state where begining of buffer is being
+       * overwritten */
+      return (size_t)(pair->write_sz - pair->read_idx + pair->write_idx);
+    } else {
+      /* simple case where has not wrapped around */
+      return (size_t)(pair->write_idx - pair->read_idx);
     }
   }
-  iotjs_string_destroy(&str);
-
-  return jerry_create_number(ret);
+  return 0;
 }
 
-static jerry_value_t tls_connect_error(iotjs_string_t* h, iotjs_string_t* p,
-                                       iotjs_string_t* hn, char* format, ...) {
-  va_list arg_list;
-  va_start(arg_list, format);
-  char* buf;
-  buf = iotjs_buffer_allocate(sizeof(char) * 256);
-  vsnprintf(buf, sizeof(char) * 256, format, arg_list);
-  va_end(arg_list);
-  iotjs_string_destroy(h);
-  iotjs_string_destroy(p);
-  iotjs_string_destroy(hn);
+int iotjs_bio_set_write_buf_size(BIO *bio, long size) {
+  if (bio == NULL || bio->type != BIO_BIO || size < 0) {
+    return SSL_FAILURE;
+  }
 
-  jerry_value_t ret_val = JS_CREATE_ERROR(COMMON, (const jerry_char_t*)buf);
-  iotjs_buffer_release(buf);
+  /* if already in pair then do not change size */
+  if (bio->pair != NULL) {
+    return SSL_FAILURE;
+  }
 
-  return ret_val;
+  bio->write_sz = (int)size;
+  if (bio->write_sz < 0) {
+    return SSL_FAILURE;
+  }
+
+  if (bio->mem != NULL) {
+    iotjs_buffer_release((char *)bio->mem);
+  }
+
+  bio->mem = (unsigned char *)iotjs_buffer_allocate((size_t)bio->write_sz);
+  if (bio->mem == NULL) {
+    return SSL_FAILURE;
+  }
+  bio->write_idx = 0;
+  bio->read_idx = 0;
+
+  return SSL_SUCCESS;
 }
 
-JS_FUNCTION(Connect) {
-  DJS_CHECK_ARGS(3, string, string, string);
-  iotjs_string_t port = JS_GET_ARG(0, string);
-  iotjs_string_t host = JS_GET_ARG(1, string);
-  iotjs_string_t hostname = JS_GET_ARG(2, string);
 
+/* Joins two BIO_BIO types. The write of b1 goes to the read of b2 and vise
+ * versa. Creating something similar to a two way pipe.
+ * Reading and writing between the two BIOs is not thread safe, they are
+ * expected to be used by the same thread. */
+int iotjs_bio_make_bio_pair(BIO *b1, BIO *b2) {
+  if (b1 == NULL || b2 == NULL) {
+    return SSL_FAILURE;
+  }
+
+  /* both are expected to be of type BIO and not already paired */
+  if (b1->type != BIO_BIO || b2->type != BIO_BIO || b1->pair != NULL ||
+      b2->pair != NULL) {
+    return SSL_FAILURE;
+  }
+
+  /* set default write size if not already set */
+  if (b1->mem == NULL &&
+      iotjs_bio_set_write_buf_size(b1, SSL_BIO_SIZE) != SSL_SUCCESS) {
+    return SSL_FAILURE;
+  }
+
+  if (b2->mem == NULL &&
+      iotjs_bio_set_write_buf_size(b2, SSL_BIO_SIZE) != SSL_SUCCESS) {
+    return SSL_FAILURE;
+  }
+
+  b1->pair = b2;
+  b2->pair = b1;
+
+  return SSL_SUCCESS;
+}
+
+
+/* Does not advance read index pointer */
+int iotjs_bio_nread0(BIO *bio, char **buf) {
+  if (bio == NULL || buf == NULL) {
+    return 0;
+  }
+
+  /* if paired read from pair */
+  if (bio->pair != NULL) {
+    BIO *pair = bio->pair;
+
+    /* case where have wrapped around write buffer */
+    *buf = (char *)pair->mem + pair->read_idx;
+    if (pair->write_idx > 0 && pair->read_idx >= pair->write_idx) {
+      return pair->write_sz - pair->read_idx;
+    } else {
+      return pair->write_idx - pair->read_idx;
+    }
+  }
+  return 0;
+}
+
+
+/* similar to SSL_BIO_nread0 but advances the read index */
+int iotjs_bio_nread(BIO *bio, char **buf, size_t num) {
+  int sz = SSL_BIO_UNSET;
+
+  if (bio == NULL || buf == NULL) {
+    return SSL_FAILURE;
+  }
+
+  if (bio->pair != NULL) {
+    /* special case if asking to read 0 bytes */
+    if (num == 0) {
+      *buf = (char *)bio->pair->mem + bio->pair->read_idx;
+      return 0;
+    }
+
+    /* get amount able to read and set buffer pointer */
+    sz = iotjs_bio_nread0(bio, buf);
+    if (sz == 0) {
+      return SSL_BIO_ERROR;
+    }
+
+    if ((int)num < sz) {
+      sz = (int)num;
+    }
+    bio->pair->read_idx += sz;
+
+    /* check if have read to the end of the buffer and need to reset */
+    if (bio->pair->read_idx == bio->pair->write_sz) {
+      bio->pair->read_idx = 0;
+      if (bio->pair->write_idx == bio->pair->write_sz) {
+        bio->pair->write_idx = 0;
+      }
+    }
+
+    /* check if read up to write index, if so then reset indexs */
+    if (bio->pair->read_idx == bio->pair->write_idx) {
+      bio->pair->read_idx = 0;
+      bio->pair->write_idx = 0;
+    }
+  }
+
+  return sz;
+}
+
+
+int iotjs_bio_nwrite(BIO *bio, char **buf, int num) {
+  int sz = SSL_BIO_UNSET;
+
+  if (bio == NULL || buf == NULL) {
+    return 0;
+  }
+
+  if (bio->pair != NULL) {
+    if (num == 0) {
+      *buf = (char *)bio->mem + bio->write_idx;
+      return 0;
+    }
+
+    if (bio->write_idx < bio->read_idx) {
+      /* if wrapped around only write up to read index. In this case
+       * read_idx is always greater then write_idx so sz will not be negative.
+       */
+      sz = bio->read_idx - bio->write_idx;
+    } else if (bio->read_idx > 0 && bio->write_idx == bio->read_idx) {
+      return SSL_BIO_ERROR; /* no more room to write */
+    } else {
+      /* write index is past read index so write to end of buffer */
+      sz = bio->write_sz - bio->write_idx;
+
+      if (sz <= 0) {
+        /* either an error has occured with write index or it is at the
+         * end of the write buffer. */
+        if (bio->read_idx == 0) {
+          /* no more room, nothing has been read */
+          return SSL_BIO_ERROR;
+        }
+
+        bio->write_idx = 0;
+
+        /* check case where read index is not at 0 */
+        if (bio->read_idx > 0) {
+          sz = bio->read_idx; /* can write up to the read index */
+        } else {
+          sz = bio->write_sz; /* no restriction other then buffer size */
+        }
+      }
+    }
+
+    if (num < sz) {
+      sz = num;
+    }
+    *buf = (char *)bio->mem + bio->write_idx;
+    bio->write_idx += sz;
+
+    /* if at the end of the buffer and space for wrap around then set
+     * write index back to 0 */
+    if (bio->write_idx == bio->write_sz && bio->read_idx > 0) {
+      bio->write_idx = 0;
+    }
+  }
+
+  return sz;
+}
+
+
+/* Reset BIO to initial state */
+int iotjs_bio_reset(BIO *bio) {
+  if (bio == NULL) {
+    /* -1 is consistent failure even for FILE type */
+    return SSL_BIO_ERROR;
+  }
+
+  switch (bio->type) {
+    case BIO_BIO:
+      bio->read_idx = 0;
+      bio->write_idx = 0;
+      return 0;
+
+    default: { break; }
+  }
+
+  return SSL_BIO_ERROR;
+}
+
+
+int iotjs_bio_read(BIO *bio, const char *buf, size_t size) {
+  int sz;
+  char *pt;
+  sz = iotjs_bio_nread(bio, &pt, size);
+
+  if (sz > 0) {
+    memset((void *)buf, 0, (size_t)sz);
+    memcpy((void *)buf, pt, (size_t)sz);
+  }
+
+  return sz;
+}
+
+int iotjs_bio_write(BIO *bio, const char *buf, size_t size) {
+  /* internal function where arguments have already been sanity checked */
+  int sz;
+  char *data;
+
+  sz = iotjs_bio_nwrite(bio, &data, size);
+
+  /* test space for write */
+  if (sz <= 0) {
+    return sz;
+  }
+
+  memset(data, 0, (size_t)sz);
+  memcpy(data, buf, (size_t)sz);
+  return sz;
+}
+
+
+/**
+ * support bio type only
+ *e
+ * @param type
+ * @return
+ */
+BIO *iotjs_ssl_bio_new(int type) {
+  BIO *bio = (BIO *)iotjs_buffer_allocate(sizeof(BIO));
+  if (bio) {
+    bzero(bio, sizeof(BIO));
+    bio->type = type;
+    bio->mem = NULL;
+    bio->prev = NULL;
+    bio->next = NULL;
+  }
+  return bio;
+}
+
+int iotjs_bio_free(BIO *bio) {
+  /* unchain?, doesn't matter in goahead since from free all */
+  if (bio) {
+    /* remove from pair by setting the paired bios pair to NULL */
+    if (bio->pair != NULL) {
+      bio->pair->pair = NULL;
+    }
+    if (bio->mem)
+      iotjs_buffer_release((char *)bio->mem);
+    iotjs_buffer_release((char *)bio);
+  }
+  return 0;
+}
+
+int iotjs_bio_free_all(BIO *bio) {
+  while (bio) {
+    BIO *next = bio->next;
+    iotjs_bio_free(bio);
+    bio = next;
+  }
+  return 0;
+}
+
+int iotjs_bio_net_send(void *ctx, const unsigned char *buf, size_t len) {
+  BIO *bio = (BIO *)ctx;
+
+  int sz;
+  sz = iotjs_bio_write(bio, (const char *)buf, len);
+  if (sz <= 0) {
+    return MBEDTLS_ERR_SSL_WANT_WRITE;
+  }
+  return sz;
+}
+
+int iotjs_bio_net_recv(void *ctx, unsigned char *buf, size_t len) {
+  BIO *bio = (BIO *)ctx;
+  int sz;
+  sz = iotjs_bio_read(bio, (const char *)buf, len);
+
+  if (sz <= 0) {
+    return MBEDTLS_ERR_SSL_WANT_READ;
+  }
+  return sz;
+}
+
+JS_FUNCTION(TlsConstructor) {
+  DJS_CHECK_THIS();
 
   jerry_value_t jtls = JS_GET_THIS();
-  iotjs_tls_t* tls_data = tls_create(jtls);
+  iotjs_tls_t *tls_data = iotjs_tls_create(jtls);
+
+  jerry_value_t opts = jargv[0];
+
+  tls_data->handshake_state = SSL_HANDSHAKE_READY;
 
   int ret = 0;
+
   if ((ret = mbedtls_ctr_drbg_seed(&tls_data->ctr_drbg, mbedtls_entropy_func,
                                    &tls_data->entropy, NULL, 0)) != 0) {
-    return tls_connect_error(&port, &host, &hostname,
-                             "drbg seeding failed, error code: %d", ret);
+    return JS_CREATE_ERROR(COMMON, "drbg_seeding_failed");
   }
 
-  ret = mbedtls_net_connect(&tls_data->server_fd, iotjs_string_data(&host),
-                            iotjs_string_data(&port), MBEDTLS_NET_PROTO_TCP);
+  jerry_value_t jca_txt = iotjs_jval_get_property(opts, "ca");
+  if (jerry_value_is_string(jca_txt)) {
+    iotjs_string_t ca_txt = iotjs_jval_as_string(jca_txt);
+    ret = mbedtls_x509_crt_parse(&tls_data->cacert,
+                                 (const unsigned char *)iotjs_string_data(
+                                     &ca_txt),
+                                 (size_t)iotjs_string_size(&ca_txt) + 1);
+
+    iotjs_string_destroy(&ca_txt);
+  } else {
+    ret = mbedtls_x509_crt_parse(&tls_data->cacert,
+                                 (const unsigned char *)SSL_CA_PEM,
+                                 sizeof(SSL_CA_PEM));
+  }
   if (ret) {
-    return tls_connect_error(&port, &host, &hostname,
-                             "failed to connect to %s:%s, error code: %d",
-                             iotjs_string_data(&host), iotjs_string_data(&port),
-                             ret);
+    return JS_CREATE_ERROR(COMMON, "x509 certificate parsing failed");
   }
-
-  ret = mbedtls_ssl_config_defaults(&tls_data->conf, MBEDTLS_SSL_IS_CLIENT,
-                                    MBEDTLS_SSL_TRANSPORT_STREAM,
-                                    MBEDTLS_SSL_PRESET_DEFAULT);
-  if (ret) {
-    return tls_connect_error(&port, &host, &hostname,
-                             "ssl config failed, error code: %d", ret);
-  }
-
-  mbedtls_ssl_conf_authmode(&tls_data->conf, MBEDTLS_SSL_VERIFY_OPTIONAL);
   mbedtls_ssl_conf_ca_chain(&tls_data->conf, &tls_data->cacert, NULL);
+
   mbedtls_ssl_conf_rng(&tls_data->conf, mbedtls_ctr_drbg_random,
                        &tls_data->ctr_drbg);
+  jerry_release_value(jca_txt);
 
-  ret = mbedtls_ssl_setup(&tls_data->ssl, &tls_data->conf);
-
-  if (ret) {
-    return tls_connect_error(&port, &host, &hostname,
-                             "ssl setup failed, error code: %d", ret);
+  if ((ret = mbedtls_ssl_config_defaults(&tls_data->conf, MBEDTLS_SSL_IS_CLIENT,
+                                         MBEDTLS_SSL_TRANSPORT_STREAM,
+                                         MBEDTLS_SSL_PRESET_DEFAULT))) {
+    return JS_CREATE_ERROR(COMMON, "SSL Configuration failed.");
   }
 
-  ret = mbedtls_ssl_set_hostname(&tls_data->ssl, iotjs_string_data(&hostname));
-  if (ret) {
-    return tls_connect_error(&port, &host, &hostname,
-                             "ssl hostname setup failed, error code: %d", ret);
+  jerry_value_t jrejectUnauthorized =
+      iotjs_jval_get_property(opts, "rejectUnauthorized");
+  bool rejectUnauthorized = iotjs_jval_as_boolean(jrejectUnauthorized);
+  mbedtls_ssl_conf_authmode(&tls_data->conf, rejectUnauthorized
+                                                 ? MBEDTLS_SSL_VERIFY_REQUIRED
+                                                 : MBEDTLS_SSL_VERIFY_NONE);
+  jerry_release_value(jrejectUnauthorized);
+
+  jerry_value_t jservername = iotjs_jval_get_property(opts, "servername");
+  iotjs_string_t servername = iotjs_jval_as_string(jservername);
+
+  size_t hostname_size = iotjs_string_size(&servername);
+  const char hostname[hostname_size + 1];
+  memset((void *)hostname, 0, hostname_size + 1);
+  memcpy((void *)hostname, iotjs_string_data(&servername), hostname_size);
+
+  iotjs_string_destroy(&servername);
+  jerry_release_value(jservername);
+  mbedtls_ssl_set_hostname(&tls_data->ssl, hostname);
+
+  if ((ret = mbedtls_ssl_setup(&tls_data->ssl, &tls_data->conf))) {
+    return JS_CREATE_ERROR(COMMON, "SSL setup failed");
   }
 
-  mbedtls_ssl_set_bio(&tls_data->ssl, &tls_data->server_fd, mbedtls_net_send,
-                      mbedtls_net_recv, NULL);
+  tls_data->app_bio = iotjs_ssl_bio_new(BIO_BIO);
+  tls_data->ssl_bio = iotjs_ssl_bio_new(BIO_BIO);
+  iotjs_bio_make_bio_pair(tls_data->ssl_bio, tls_data->app_bio);
+  mbedtls_ssl_set_bio(&tls_data->ssl, tls_data->ssl_bio, iotjs_bio_net_send,
+                      iotjs_bio_net_recv, NULL);
 
-  while ((ret = mbedtls_ssl_handshake(&tls_data->ssl))) {
-    if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-      return tls_connect_error(&port, &host, &hostname,
-                               "handshake failed, error code: -0x%x", -ret);
+
+  return jerry_create_undefined();
+}
+
+void iotjs_tls_update(iotjs_tls_t *tls_data) {
+  size_t pending = iotjs_bio_ctrl_pending(tls_data->app_bio);
+  if (pending > 0) {
+    char src[pending];
+    iotjs_bio_read(tls_data->app_bio, src, sizeof(src));
+
+    jerry_value_t jthis = tls_data->jobject;
+    jerry_value_t fn = iotjs_jval_get_property(jthis, "onwrite");
+    iotjs_jargs_t jargv = iotjs_jargs_create(1);
+    jerry_value_t jbuffer = iotjs_bufferwrap_create_buffer((size_t)pending);
+    iotjs_bufferwrap_t *buffer_wrap = iotjs_bufferwrap_from_jbuffer(jbuffer);
+
+    iotjs_bufferwrap_copy(buffer_wrap, (const char *)src, pending);
+    iotjs_jargs_append_jval(&jargv, jbuffer);
+    iotjs_make_callback(fn, jthis, &jargv);
+
+    jerry_release_value(fn);
+    jerry_release_value(jbuffer);
+    iotjs_jargs_destroy(&jargv);
+  }
+}
+
+int iotjs_tls_error_handler(iotjs_tls_t *tls_data, const int code) {
+  if (code == MBEDTLS_ERR_SSL_WANT_READ || code == MBEDTLS_ERR_SSL_WANT_WRITE) {
+    iotjs_tls_update(tls_data);
+  } else if (code < 0) {
+    printf("mbedtls handshake failed");
+  }
+
+  return code;
+}
+
+JS_FUNCTION(Handshake) {
+  JS_DECLARE_THIS_PTR(tls, tls_data);
+
+  if (tls_data->handshake_state == SSL_HANDSHAKE_DONE) {
+    return jerry_create_number(SSL_HANDSHAKE_DONE);
+  }
+  tls_data->handshake_state = SSL_HANDSHAKE_IN_PROGRESS;
+
+  int ret_val = 0;
+  ret_val = mbedtls_ssl_handshake(&tls_data->ssl);
+  ret_val = iotjs_tls_error_handler(tls_data, ret_val);
+
+  if (!ret_val) {
+    tls_data->handshake_state = SSL_HANDSHAKE_DONE;
+
+    int verify_status = (int)mbedtls_ssl_get_verify_result(&tls_data->ssl);
+    if (verify_status) {
+      char buf[256];
+      mbedtls_x509_crt_verify_info(buf, sizeof(buf), "::",
+                                   (uint32_t)verify_status);
+      printf("%s\n", buf);
     }
+
+    jerry_value_t fn = iotjs_jval_get_property(jthis, "onhandshakedone");
+    iotjs_make_callback(fn, jthis, iotjs_jargs_get_empty());
+    jerry_release_value(fn);
   }
 
-  iotjs_string_destroy(&host);
-  iotjs_string_destroy(&port);
-  iotjs_string_destroy(&hostname);
+  return jerry_create_number(tls_data->handshake_state);
+}
+
+JS_FUNCTION(Write) {
+  JS_DECLARE_THIS_PTR(tls, tls_data);
+
+  iotjs_bufferwrap_t *buf = iotjs_bufferwrap_from_jbuffer(jargv[0]);
+  size_t ret_val =
+      (size_t)mbedtls_ssl_write(&tls_data->ssl, (const unsigned char *)buf,
+                                iotjs_bufferwrap_length(buf));
+  size_t pending = 0;
+  if ((pending = iotjs_bio_ctrl_pending(tls_data->app_bio)) > 0) {
+    const char temp_buffer[pending];
+    memset((void *)temp_buffer, 0, pending);
+    ret_val = (size_t)iotjs_bio_read(tls_data->app_bio, temp_buffer, pending);
+
+    jerry_value_t out = iotjs_bufferwrap_create_buffer(ret_val);
+    iotjs_bufferwrap_t *outbuf = iotjs_bufferwrap_from_jbuffer(out);
+    iotjs_bufferwrap_copy(outbuf, (const char *)temp_buffer, ret_val);
+    return out;
+  } else {
+    return jerry_create_null();
+  }
+}
+
+JS_FUNCTION(End) {
+  JS_DECLARE_THIS_PTR(tls, tls_data);
+
+  iotjs_tls_destroy(tls_data);
+  iotjs_bio_free_all(tls_data->app_bio);
+  iotjs_bio_free_all(tls_data->ssl_bio);
 
   return jerry_create_undefined();
 }
 
 jerry_value_t InitTls() {
   jerry_value_t jtls = jerry_create_object();
+  jerry_value_t tlsConstructor = jerry_create_external_function(TlsConstructor);
 
-  iotjs_jval_set_method(jtls, IOTJS_MAGIC_STRING_CONNECT, Connect);
-  iotjs_jval_set_method(jtls, IOTJS_MAGIC_STRING_WRITE, Write);
+  iotjs_jval_set_property_jval(jtls, IOTJS_MAGIC_STRING_TLSWRAP,
+                               tlsConstructor);
+
+  jerry_value_t prototype = jerry_create_object();
+
+  iotjs_jval_set_method(prototype, IOTJS_MAGIC_STRING_WRITE, Write);
+  iotjs_jval_set_method(prototype, "handshake", Handshake);
+  iotjs_jval_set_method(prototype, "end", End);
+  iotjs_jval_set_property_jval(tlsConstructor, "prototype", prototype);
+
+  jerry_release_value(prototype);
+  jerry_release_value(tlsConstructor);
 
   return jtls;
 }
--- src/modules/iotjs_module_tls.h
+++ src/modules/iotjs_module_tls.h
@@ -25,14 +25,147 @@
 #include "mbedtls/net_sockets.h"
 #include "mbedtls/ssl.h"
 
+const char SSL_CA_PEM[] =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4GA1UECxMX\n"
+    "R2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNpZ24xEzARBgNVBAMT\n"
+    "Ckdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1MDgwMDAwWjBMMSAwHgYDVQQL\n"
+    "ExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEGA1UEChMKR2xvYmFsU2lnbjETMBEGA1UE\n"
+    "AxMKR2xvYmFsU2lnbjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8o\n"
+    "mUVCxKs+IVSbC9N/hHD6ErPLv4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7\n"
+    "SqbKSaZeqKeMWhG8eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQ\n"
+    "BoZfXklqtTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd\n"
+    "C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pazq+r1feq\n"
+    "CapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCBmTAOBgNVHQ8BAf8E\n"
+    "BAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IHV2ccHsBqBt5ZtJot39wZhi4w\n"
+    "NgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5nbG9iYWxzaWduLm5ldC9yb290LXIyLmNy\n"
+    "bDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEA\n"
+    "mYFThxxol4aR7OBKuEQLq4GsJ0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkI\n"
+    "k7mpM0sYmsL4h4hO291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRD\n"
+    "LenVOavSot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd\n"
+    "AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7TBj0/VLZ\n"
+    "jmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg==\n"
+    "-----END CERTIFICATE-----\n";
+enum {
+  SSL_HANDSHAKE_READY = 0,
+  SSL_HANDSHAKE_IN_PROGRESS = 1,
+  SSL_HANDSHAKE_DONE = 2,
+};
+
+enum { /* ssl Constants */
+       SSL_ERROR_NONE = 0,
+       SSL_FAILURE = 0,
+       SSL_SUCCESS = 1,
+       SSL_SHUTDOWN_NOT_DONE = 2,
+
+       SSL_ALPN_NOT_FOUND = -9,
+       SSL_BAD_CERTTYPE = -8,
+       SSL_BAD_STAT = -7,
+       SSL_BAD_PATH = -6,
+       SSL_BAD_FILETYPE = -5,
+       SSL_BAD_FILE = -4,
+       SSL_NOT_IMPLEMENTED = -3,
+       SSL_UNKNOWN = -2,
+       SSL_FATAL_ERROR = -1,
+
+       SSL_FILETYPE_ASN1 = 2,
+       SSL_FILETYPE_PEM = 1,
+       SSL_FILETYPE_DEFAULT = 2, /* ASN1 */
+       SSL_FILETYPE_RAW = 3,     /* NTRU raw key blob */
+
+       SSL_VERIFY_NONE = 0,
+       SSL_VERIFY_PEER = 1,
+       SSL_VERIFY_FAIL_IF_NO_PEER_CERT = 2,
+       SSL_VERIFY_CLIENT_ONCE = 4,
+       SSL_VERIFY_FAIL_EXCEPT_PSK = 8,
+
+       SSL_SESS_CACHE_OFF = 0x0000,
+       SSL_SESS_CACHE_CLIENT = 0x0001,
+       SSL_SESS_CACHE_SERVER = 0x0002,
+       SSL_SESS_CACHE_BOTH = 0x0003,
+       SSL_SESS_CACHE_NO_AUTO_CLEAR = 0x0008,
+       SSL_SESS_CACHE_NO_INTERNAL_LOOKUP = 0x0100,
+       SSL_SESS_CACHE_NO_INTERNAL_STORE = 0x0200,
+       SSL_SESS_CACHE_NO_INTERNAL = 0x0300,
+
+       SSL_ERROR_WANT_READ = 2,
+       SSL_ERROR_WANT_WRITE = 3,
+       SSL_ERROR_WANT_CONNECT = 7,
+       SSL_ERROR_WANT_ACCEPT = 8,
+       SSL_ERROR_SYSCALL = 5,
+       SSL_ERROR_WANT_X509_LOOKUP = 83,
+       SSL_ERROR_ZERO_RETURN = 6,
+       SSL_ERROR_SSL = 85,
+
+       SSL_SENT_SHUTDOWN = 1,
+       SSL_RECEIVED_SHUTDOWN = 2,
+       SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER = 4,
+       SSL_OP_NO_SSLv2 = 8,
+
+       SSL_R_SSL_HANDSHAKE_FAILURE = 101,
+       SSL_R_TLSV1_ALERT_UNKNOWN_CA = 102,
+       SSL_R_SSLV3_ALERT_CERTIFICATE_UNKNOWN = 103,
+       SSL_R_SSLV3_ALERT_BAD_CERTIFICATE = 104,
+       PEM_BUFSIZE = 1024
+};
+
+struct _BIO;
+typedef struct _BIO BIO;
+
+struct _BIO {
+  BIO *prev;
+  BIO *next;
+  BIO *pair;
+  unsigned char *mem;
+  int write_sz;
+  int write_idx;
+  int read_idx;
+  int read_rq;
+  int mem_len;
+  int type;
+};
+
+enum {
+  SSL_BIO_ERROR = -1,
+  SSL_BIO_UNSET = -2,
+  SSL_BIO_SIZE = 17000,
+};
+
+enum BIO_TYPE {
+  BIO_BUFFER = 1,
+  BIO_SOCKET = 2,
+  BIO_SSL = 3,
+  BIO_MEMORY = 4,
+  BIO_BIO = 5,
+  BIO_FILE = 6
+};
+
+BIO *iotjs_ssl_bio_new(int type);
+int iotjs_bio_make_bio_pair(BIO *b1, BIO *b2);
+
+size_t iotjs_bio_ctrl_pending(BIO *bio);
+int iotjs_bio_read(BIO *bio, const char *buf, size_t size);
+int iotjs_bio_write(BIO *bio, const char *buf, size_t size);
+
+int iotjs_bio_reset(BIO *bio);
+int iotjs_bio_net_recv(void *ctx, unsigned char *buf, size_t len);
+int iotjs_bio_net_send(void *ctx, const unsigned char *buf, size_t len);
+int iotjs_bio_free_all(BIO *bio);
+int iotjs_bio_free(BIO *bio);
+
 typedef struct iotjs_tls_t {
   jerry_value_t jobject;
-  mbedtls_net_context server_fd;
+
   mbedtls_entropy_context entropy;
   mbedtls_ctr_drbg_context ctr_drbg;
+
   mbedtls_ssl_context ssl;
   mbedtls_ssl_config conf;
   mbedtls_x509_crt cacert;
+  int handshake_state;
+
+  BIO *ssl_bio;
+  BIO *app_bio;
 } iotjs_tls_t;
 
 #endif /* IOTJS_MODULE_TLS_H */
